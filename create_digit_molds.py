2f7f976f70a9125fdec5d8b794c92f15bc2eaed4
Arno Khachatourian <arno.kha@gmail.com>
2017-02-01 07:18:33 -0500

    Added method to create molds with regularized pixel intensities

--- create_digit_molds.py
+++ create_digit_molds.py
@@ -19,6 +19,7 @@ def count_labels(labels):
 	return label_count
 
 def create_molds():
+	""" Create molds of numbers by averaging the training examples """
 	label_counter = np.zeros( (mnist.n_labels, 1), dtype=np.uint16 )  # Used for mold averaging
 	molds = np.zeros( (mnist.n_labels, mnist.img_dimensions), dtype=np.uint32 )
 
@@ -35,8 +36,27 @@ def create_molds():
 
 	return molds
 
+def create_regularized_molds():
+	""" Create molds of numbers by averaging the training examples, 
+	and make sure they have the same overall pixel intensity 
+	"""
+	r_molds = create_molds() # Start with the non-regularized molds
+	mold_pixel_count = np.zeros( (mnist.n_labels, 1), dtype=np.uint16 )
+	for label in range(mnist.n_labels):
+		for pixel in range(mnist.img_dimensions):
+			mold_pixel_count[label] += r_molds[label,pixel]
+
+	# Regularize
+	mold_regulaizer = mold_pixel_count / 10000
+	for label in range(mnist.n_labels):
+		for pixel in range(mnist.img_dimensions):
+			r_molds[label,pixel] = r_molds[label,pixel] / mold_regulaizer[label]
+
+	return r_molds
+
 def main():
-	create_molds()
+	## TODO change to pass() create_molds()
+	create_regularized_molds()
 
 if __name__ == "__main__":
 	main()
