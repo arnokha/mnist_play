947f2df65ce05f9bfb15e7518c25ac648cdf4756
Arno Khachatourian <arno.kha@gmail.com>
2017-02-04 07:04:32 -0500

    Finished kNN... just need to clean it up

--- k_NN_mnist.py
+++ k_NN_mnist.py
@@ -1,50 +1,50 @@
+##
 ## Use kNN for mnist
+## Not using weighted k-NN. Only going to use the mode of k-NNs or return the first value if no mode exists
+##
+## Author: Arno Khachatourian
+##
 
 import numpy as np
+from scipy import stats
 import extract_mnist_data as mnist
 ## Now have access to x_train, y_train, x_test, y_test, n_train, n_test, img_dimensions, and n_labels
 
-#def weight():
-
 def distance(x1, x2):
+	""" This distance function just takes the absolute value of the differences in pixel intensities between these two images """
 	return np.sum(abs(x1 - x2))
 
 def kNN(k):
-	pass
+	""" Uses k-NN to return predictions based on the labels of the k nearest neighbors. Does not use weighting """
 
-def NN():
-	k = 1
-	knn_x_train = np.zeros((mnist.n_train, k), dtype=np.uint8)
-	d = np.zeros((mnist.n_train, mnist.n_train), dtype=np.uint16)
-	max_distance = 2 ** 16 - 1
+	## Initialize variables
+	max_distance = 28 * 28 * 255
+	d = np.zeros((mnist.n_test, mnist.n_train), dtype=np.uint32) # Max value is 28 * 28 * 255 = 199,920
+	nearest_neighbors = np.zeros((mnist.n_train, k), dtype=np.uint16) # Max value is 59,999
+	labels_of_nearest_neighbors = np.zeros((mnist.n_train, k), dtype=np.uint8) # Max value is 9
+	predictions = np.zeros((mnist.n_train, 1), dtype=np.uint8) # Max value is 9
 
-#	for i in range(mnist.n_train):
-	for i in range(50):
+	##
+	## Loop through test set, calculate distance between each test and training example, 
+	## find k nearest neighbors, and use the mode of their labels to predict the label of the test example
+	##
+	for i in range(mnist.n_test):
 		for neighbor in range(mnist.n_train):
-			d[i,neighbor] = distance( mnist.x_train[i,:], mnist.x_train[neighbor,:] )
+			d[i,neighbor] = distance( mnist.x_test[i,:], mnist.x_train[neighbor,:] )
 
-		d[i,i] = max_distance ## Commenting this out should make this 100% accturate on training set when k = 1
-#		if i % 1000 == 0:
-#			print(str(i))
 		for k_index in range(k):
 			index_min_distance = np.argmin(d[i,:])
 			d[i,index_min_distance] = max_distance ## Set to max so next iteration wont pick again... need to remove if I'm gonna use weighting
-			knn_x_train[i,k_index] = index_min_distance
+			nearest_neighbors[i,k_index] = index_min_distance
+			labels_of_nearest_neighbors[i,k_index] = mnist.y_train[ nearest_neighbors[i,k_index] ]
 
-		print( "Nearest neighbor of x_train #" + str(i) + " is " + str(knn_x_train[i]) )
-		print( "Label of nearest neighbor of x_train is " + str( mnist.y_train[ knn_x_train[i] ] ) )
-		print( "Actual label of x_train #" + str(i) + " is " + str( mnist.y_train[i] ) )
+		predictions[i] = stats.mode(labels_of_nearest_neighbors[i], axis=None)[0][0]
+		print( str(predictions[i]) + "," + str(mnist.y_test[i]) )
 
-#	for i in range(mnist.n_train):
-#		if i % 1000 == 0:
-#			print( "Nearest neighbor of x_train #" + str(i) + " is " + str(knn_x_train[i,k]) )
-#			print( "Label of nearest neighbor of x_train is " + str( mnist.y_train[ knn_x_train[i,k] ] ) )
-#			print( "Actual label of x_train #" + str(i) + " is " + str( mnist.y_train[i] ) )
+	return predictions
 
 def main():
-	NN() ## This is just to help me implement kNN
 
-	exit() ## TODO remove garbage
 	k = int(input("Enter an odd number to use for k-Nearest-Neighbors: "))
 	if ( k % 2 == 0): 
 		## Number is even. Prompt user for an odd number
@@ -52,10 +52,6 @@ def main():
 		exit()
 	kNN(k)
 
-	print("Testing distance function...") ##TODO remove following lines
-	x1 = np.ones((2,2))
-	x2 =  - np.ones((2,2))
-	print(str(distance(x1,x2)))
 
 if __name__ == "__main__":
 	main()
